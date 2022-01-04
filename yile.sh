48589b20db2b66740a8e69a5c3efe6860d527a01
yuweng1013 <97008584+yuweng1013@users.noreply.github.com>
2022-01-04 15:26:21 +0800

    Update yile.sh

--- yile.sh
+++ yile.sh
@@ -1,1 +1,29 @@
 wget -O PowerMTA-4.5r11.rpm https://github.com/yuweng1013/autoinstall/raw/main/PowerMTA-4.5r11.rpm
+rpm -Uvh PowerMTA-4.5r11.rpm
+wget -O pmta https://github.com/yuweng1013/autoinstall/raw/main/pmta
+wget -O pmtad https://github.com/yuweng1013/autoinstall/raw/main/pmtad
+wget -O pmtahttpd https://github.com/yuweng1013/autoinstall/raw/main/pmtahttpd
+wget -O pmtasnmpd https://github.com/yuweng1013/autoinstall/raw/main/pmtasnmpd
+wget -O license https://github.com/yuweng1013/autoinstall/raw/main/license
+wget -O config https://github.com/yuweng1013/autoinstall/raw/main/config
+wget -O mykey.6068805.com.pem https://github.com/yuweng1013/autoinstall/raw/main/mykey.6068805.com.pem
+
+echo -n "your pmta ip:"
+read pmtaip
+echo -n "your pmta hostname:"
+read pmtahostname
+echo -n "your pmta port:"
+read pmtaport
+
+\cp license /etc/pmta/
+\cp mykey.6068805.com.pem /etc/pmta/mykey.$pmtahostname.pem
+\cp pmta /usr/sbin/pmta
+\cp pmtad /usr/sbin/pmtad
+\cp pmtahttpd /usr/sbin/pmtahttpd
+\cp pmtasnmpd /usr/sbin/pmtasnmpd
+
+sed -i "s/QQQipQQQ/$pmtaip/g" `grep "QQQipQQQ" -rl /etc/pmta/config`
+sed -i "s/QQQhostnameQQQ/$pmtaip/g" `grep "QQQhostnameQQQ" -rl /etc/pmta/config`
+sed -i "s/QQQportQQQ/$pmtaip/g" `grep "QQQportQQQ" -rl /etc/pmta/config`
+
+service pmta restart
